bab5cd1e4c775f05721f782bc299e44452082447
tangpaak <tangpaak@gmail.com>
2023-07-11 17:59:44 +0800

    bc-exercise2

--- bc-exercise2/day2/JavaQuest1.java
+++ bc-exercise2/day2/JavaQuest1.java
@@ -31,13 +31,14 @@ public class JavaQuest1 {
  
 
     // Add two numbers and print it out
-    System.out.println(first+second);
     // code here
+    int sum = first + second;
     System.out.println("The sum is " + sum);
 
     // subtraction
     // code here with tips, pls fix
     // subtract = first - second
+    int subtract = second - first;
     System.out.println("The subtraction reuslt is " + subtract);
 
     // another sum
@@ -45,10 +46,13 @@ public class JavaQuest1 {
     byte b2 = 70;
     // code here with tips, pls fix
     // sum = b1 + b2;
+    sum = b1 + b2;
     System.out.println("The another sum reuslt is " + sum);
-
+    
     // declare two boolean variable with true and false value
     // code here
+    boolean varB = false;
+    boolean varC = true;
     System.out.println("The variable varB is " + varB);
     System.out.println("The variable varC is " + varC);
 
@@ -56,6 +60,8 @@ public class JavaQuest1 {
     float fnumber = -130.20f; // By default, numbers are treated as double in Java.
     // Use suffix 'f' with value to make it float.
     // code here 
+    double dnumber = -20.5;
+
     System.out.println("The variable fnumber is " + fnumber);
     System.out.println("The variable dnumber is " + dnumber);
 
@@ -63,15 +69,18 @@ public class JavaQuest1 {
     int a = 7;
     int b = 3;
     // Fix it
-    int reminder = a - b;
+    int reminder = a % b;
     System.out.println("The variable reminder is " + reminder);
 
     // declare a variable x and assign value to it.
     int e = 7;
     int o = 8;
+    int x = 2;
     int y = 4;
     // code here
     int result = e - o * x / y;
-    System.out.println("The result is " + result);
+    System.out.println("The variable result is " + result);
+
+
   }
 }
