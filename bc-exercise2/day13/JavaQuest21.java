0f187d115bd4246ff768a9f1ee70e5717b8fb9c4
tangpaak <tangpaak@gmail.com>
2023-07-30 23:31:56 +0800

    update

---
+++ bc-exercise2/day13/JavaQuest21.java
@@ -0,0 +1,61 @@
+/*
+ * 
+ * Question : Given an integer n, return true if n has exactly three positive divisors. Otherwise, return false.
+ * 
+ * An integer m is a divisor of n if there exists an integer k such that n = k * m.
+ * 
+ */
+public class JavaQuest21 {
+  public static void main(String[] args) {
+    System.out.println(isThreeDivisors(1)); // false 1/1
+    System.out.println(isThreeDivisors(3)); // false 3/1, 3/3
+    System.out.println(isThreeDivisors(4)); // true, 4/1, 4/2, 4/4
+    System.out.println(isThreeDivisors(12)); // false 12/1, 12/2, 12/4, 12/6
+    System.out.println(isThreeDivisors(9999)); // false
+    System.out.println(isThreeDivisors(9)); // true, 9/1, 9/3, 9/9
+    System.out.println(isThreeDivisors(25)); //true, 25/1, 25/5, 25/25
+    System.out.println(isThreeDivisors(49)); //true
+    System.out.println(isThreeDivisors(81));
+
+  }
+
+  // public static boolean isThreeDivisors(int n) {
+  //   // finish the code
+  //   if(n == 1){
+  //     return false;
+  //   }
+
+  // for (int i = 2; i < n; i++) {
+  //     if (n % i == 0 && Math.pow(i, i) == n && n % n == 0) {
+  //         return true;
+  //     }
+  // }
+  // return false;
+
+  //   double a = Math.round(Math.sqrt(n));
+  //   boolean num = (n % n == 0 && n % 1 == 0 && n % a == 0 && n !=1);
+
+  //   if (num) {
+  //         return true;
+  //       }
+
+  //     return false;
+  // }
+
+    public static boolean isThreeDivisors(int n) {
+      int count = 0;
+      for (int i = 1; i <= n; i++) {
+          if (n % i == 0) {
+              count++;
+          }
+          if (count > 3) {
+              return false;
+          }
+      }
+      return count == 3;
+  }
+
+
+
+  }
+
---
+++ bc-exercise2/day13/JavaQuest22.java
@@ -0,0 +1,98 @@
+
+// Question : Given an integer number n, return the difference between the product
+// of its digits and the sum of its digits.
+
+// Constraints : 1 <= n <= 10^5
+
+// Example :
+
+// Input: n = 4568 Output: 937 Explanation:
+// Product of digits = 4 * 5 * 6 * 8 = 960
+// Sum of digits = 4 + 5 + 6 + 8 = 23
+// Result = 960 - 23 = 937
+
+import java.util.Arrays;
+
+public class JavaQuest22 {
+  public static void main(String[] args) {
+    System.out.println(findDifferent(1)); // output : 0
+    System.out.println(findDifferent(100000));// output : -1
+    System.out.println(findDifferent(234)); // output : 15
+    System.out.println(findDifferent(4568));// output : 937
+    System.out.println(findDifferent(9999));// output : 6525
+    System.out.println(findDifferent(11111));// output : -4
+    System.out.println(findDifferent(0));// output : 0
+  }
+
+  public static int findDifferent(int n) {
+    if (n == 0) {
+      return 0;
+    }
+
+    // code here
+    String str = String.valueOf(n);
+    int[] arr = new int[str.length()];
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = Character.getNumericValue(str.charAt(i));
+    }
+
+    int pd = 1;
+    int sd = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+      if (arr.length == 1) {
+        pd = arr[i];
+      }
+
+      else if (arr.length > 0) {
+        for (i = 0; i < arr.length; i++) {
+          if (i == 0) {
+            pd = arr[i];
+          }
+          if (i != arr.length - 1) {
+            pd = pd * arr[i + 1];
+            // System.out.println("pd= " + pd);
+            // System.out.println("arr[i]=" + arr[i]);
+            // System.out.println("arr[i+1]=" + arr[i + 1]);
+          } else if (i == arr.length) {
+            pd = pd * arr.length - i - 1;
+          }
+          // System.out.println(arr[i]);
+        }
+      }
+
+      for (i = 0; i < str.length(); i++) {
+        sd = sd + arr[i];
+      }
+    }
+
+    // System.out.println("sd=" + sd);
+    int num = pd - sd;
+    return num;
+  }
+
+  // Another ans: 
+  // public static int findDifferent(int n) {
+  // if (n == 0) {
+  // return 0;
+  // }
+
+  // String str = String.valueOf(n);
+  // int[] arr = new int[str.length()];
+  // for (int i = 0; i < arr.length; i++) {
+  // arr[i] = Character.getNumericValue(str.charAt(i));
+  // }
+
+  // int pd = 1;
+  // int sd = 0;
+
+  // for (int i = 0; i < arr.length; i++) {
+  // pd *= arr[i];
+  // sd += arr[i];
+  // }
+
+  // return pd - sd;
+  // }
+
+
+}
