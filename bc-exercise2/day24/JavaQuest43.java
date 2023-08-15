2b435239ff494e06c8400f16cbba94f6e1423ce4
tangpaak <tangpaak@gmail.com>
2023-08-15 22:35:40 +0800

    update

--- bc-exercise2/day24/JavaQuest43.java
+++ bc-exercise2/day24/JavaQuest43.java
@@ -1,5 +1,59 @@
-package bc-exercise2.day24;
+
+
+/*
+ * There is an undirected star graph consisting of n nodes labeled from 1 to n. 
+ * A star graph is a graph where there is one center node and exactly n - 1 edges that connect the center node with every
+ * other node.
+ * 
+ * You are given a 2D integer array edges where each edges[i] = [ui, vi] indicates that there is an edge between the nodes ui and vi. 
+ * Return the center of the given star graph.
+ * 
+ * 
+ * Example 1: Input: edges = [[1,2],[2,3],[4,2]] Output: 2 
+ * Explanation: As shown in the figure above, node 2 is connected to every other node, so 2 is the center.
+ * 
+ * Example 2: Input: edges = [[1,2],[5,1],[1,3],[1,4]] Output: 1
+ * 
+ * Constraints:
+ * 
+ * 3 <= n <= 105 edges.length == n - 1 .//
+ * edges[i].length == 2 .//
+ * 1 <= ui, vi <= n ,//
+ * ui != vi ,//
+ * The given edges represent a valid star graph.
+ */
 
 public class JavaQuest43 {
-  
+  public static void main(String[] args) {
+    System.out.println(findCenter(new int[][] {{1, 2}, {2, 3}, {4, 2}})); // output : 2
+    System.out.println(findCenter(new int[][] {{1, 2}, {5, 1}, {1, 3},{1,4}})); // output : 1
+    System.out.println(findCenter(new int[][] {{4, 3}, {2, 3}, {1, 3},{3,5},{3,6},{3,7}})); // output : 3
+
+  }
+
+  public static int findCenter(int[][] edges) {
+    int n = edges.length + 1; // Number of nodes in the graph , connect to center node
+    System.out.println("edge= " + edges.length);
+
+    // Create a frequency map to count the occurrences of each node
+    int[] frequency = new int[n + 1];
+
+    // Traverse the edges
+    for (int[] edge : edges) {
+        int u = edge[0];
+        int v = edge[1];
+        frequency[u]++;
+        frequency[v]++;
+
+        // Check if any node has occurred twice
+        if (frequency[u] == 2) {
+            return u;
+        }
+        if (frequency[v] == 2) {
+            return v;
+        }
+    }
+    return -1; // Return -1 if the given edges do not form a valid star graph
+  }
+
 }
