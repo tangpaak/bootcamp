1e37e01bd67639f37755dcfb3399fb400fbff9f5
tangpaak <tangpaak@gmail.com>
2023-08-15 22:36:18 +0800

    update

--- bc-exercise2/day25/JavaQuest45.java
+++ bc-exercise2/day25/JavaQuest45.java
@@ -1,5 +1,46 @@
-package bc-exercise2.day25;
+
+
+/*
+ * Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+ * 
+ * Note that after backspacing an empty text, the text will continue empty.
+ */
+
+import java.util.Stack;
 
 public class JavaQuest45 {
-  
+  public static void main(String[] args) {
+    System.out.println(backspaceCompare("ab#c", "ad#c")); //true
+    System.out.println(backspaceCompare("ab##", "c#d#")); //true
+    System.out.println(backspaceCompare("a#c", "b")); //false
+
+  }
+
+  // Approach 1
+  public static boolean backspaceCompare(String s, String t) {
+    return buildString(s).equals(buildString(t));
 }
+
+private static String buildString(String str) {
+    Stack<Character> stack = new Stack<>();
+
+    for (char ch : str.toCharArray()) {
+        if (ch != '#') {
+            stack.push(ch);
+        } else if (!stack.isEmpty()) {
+            stack.pop();
+        }
+    }
+
+    StringBuilder sb = new StringBuilder();
+    while (!stack.isEmpty()) {
+        sb.append(stack.pop());
+
+        // System.out.println(sb);
+    }
+
+    return sb.toString();
+}
+}
+
+
--- bc-exercise2/day25/JavaQuest46.java
+++ bc-exercise2/day25/JavaQuest46.java
@@ -1,5 +1,34 @@
-package bc-exercise2.day25;
-
+/*
+ * Given an integer array nums, return the greatest common divisor of the smallest number and largest number in nums.
+ * 
+ * The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+ * 
+ */
 public class JavaQuest46 {
-  
+  public static void main(String[] args) {
+    System.out.println(findGCD(new int[] {2, 5, 6, 9, 10}));
+    System.out.println(findGCD(new int[] {7, 5, 6, 8, 3}));
+    System.out.println(findGCD(new int[] {3, 3}));
+
+  }
+
+  public static int findGCD(int[] nums) {
+    int smallest = Integer.MAX_VALUE;
+    int largest = Integer.MIN_VALUE;
+
+    for (int num : nums) {
+        smallest = Math.min(smallest, num);
+        largest = Math.max(largest, num);
+    }
+
+    return gcd(smallest, largest);
 }
+
+private static int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+}
+
